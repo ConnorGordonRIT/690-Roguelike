1853b6953cc7111631ca641a60590ddd2d4d0057
ira8296 <ira8296@g.rit.edu>
2021-09-30 22:09:54 -0400

    Created Player class

---
+++ Roguelike/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roguelike
+{
+    class Player
+    {
+        //Player fields
+        private int level;
+        private GameObject token;
+
+    }
+}
