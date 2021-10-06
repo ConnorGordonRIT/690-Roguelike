6638d261d241ddeff4fcb3c03ebcb94dae358c26
ira8296 <ira8296@g.rit.edu>
2021-10-06 16:39:54 -0400

    Further implemented SvrChatbox

--- Roguelike/SvrChatbox.cs
+++ Roguelike/SvrChatbox.cs
@@ -6,26 +6,68 @@ using System.Text;
 
 namespace Roguelike {
     class SvrChatbox {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont textStyle;
+        String text;
+
         private GameObject textBox;
         private GameObject chatLog;
         
-        public SvrChatbox(Texture2D boxSprite, Texture2D logSprite, Rectangle frame, Rectangle area)
+        public SvrChatbox(Texture2D boxSprite, Texture2D logSprite, Rectangle area, SpriteFont font)
         {
+            textStyle = font;
+            Rectangle frame = new Rectangle(0, 0, 100, 100);
+
+            int originalY = area.Y;
+            int originalHeight = area.Height;
+
+            area.Y = area.Height - 10;
+            area.Height = 10;
+
             textBox = new GameObject(boxSprite, frame, area);
-            /* Further tweak the text box's frame and area here */
+
+            area.Y = originalY;
+            area.Height = originalHeight - 10;
+
 
             chatLog = new GameObject(logSprite, frame, area);
-            /* Further tweak the text box's frame and area here */
+
+            textBox.Draw(spriteBatch);
+            chatLog.Draw(spriteBatch);
+            
+
         }
 
+
+
+        //Displays player text on the chat log
         public void Register(string input)
         {
+            String line = String.Empty;
+            String returnString = String.Empty;
+            String[] wordArray = input.Split(' ');
 
+            foreach(String word in wordArray)
+            {
+                if(textStyle.MeasureString(line + word).Length() > chatLog.ObjArea.Width)
+                {
+                    returnString = returnString + line + '\n';
+                    line = String.Empty;
+                }
+
+                line = line + word + ' ';
+            }
+
+            returnString = returnString + line;
+
+            spriteBatch.DrawString(textStyle, returnString, new Vector2(chatLog.objPos.X, chatLog.objPos.Y), Color.White);
         }
 
+        //Sends the player text to other players
         public string Post(string input)
         {
-            return "";
+
         }
     }
 }
